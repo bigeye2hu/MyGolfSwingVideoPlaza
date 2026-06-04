4a3edefd73fd2f0cc18654627a2b4cd5eaed9a97
bigeye2hu <372931693@qq.com>
2026-06-04 17:37:34 +0800

    Disable admin page caching

--- admin-server/server.py
+++ admin-server/server.py
@@ -19,6 +19,12 @@ class AdminHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, directory=ADMIN_DIR, **kwargs)
 
+    def end_headers(self):
+        self.send_header("Cache-Control", "no-store, no-cache, must-revalidate, max-age=0")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+        super().end_headers()
+
     def log_message(self, *args):
         pass
 
